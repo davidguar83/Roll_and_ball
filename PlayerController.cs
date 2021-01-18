b26f57cc879cccefa469abef65e0bc906f55762f
Davidguar83 <davidrguardiola@hotmail.es>
2021-01-18 16:44:43 +0100

    control player

---
+++ PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject WinTextObject;
+
+    private float movementX;
+    private float movementY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetConutText();
+        WinTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+
+    void SetConutText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+        if (count >= 25)
+        {
+            WinTextObject.SetActive(true);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetConutText();
+
+        }
+       
+
+    }
+
+
+}
