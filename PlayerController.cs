097c7b8cd88da3c4adb8adc51c6460b48f255241
Davidguar83 <davidrguardiola@hotmail.es>
2021-01-21 21:46:35 +0100

    actualizacion

--- PlayerController.cs
+++ PlayerController.cs
@@ -11,17 +11,23 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject WinTextObject;
+  
 
     private float movementX;
     private float movementY;
+   
 
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
         count = 0;
 
         SetConutText();
+
         WinTextObject.SetActive(false);
+     
     }
 
     void OnMove(InputValue movementValue)
@@ -31,6 +37,9 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+
+        //mensaje consola
+        Debug.Log("Stoy en OnMove");
 
     }
 
@@ -42,33 +51,54 @@ public class PlayerController : MonoBehaviour
         if (count >= 25)
         {
             WinTextObject.SetActive(true);
+          
         }
     }
 
 
     void FixedUpdate()
     {
-
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+     
+            Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
 
 
-    }
+  
 
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
             SetConutText();
 
         }
+        /* if (other.gameObject.CompareTag("Enemy")) { 
        
+           // this.transform.position = startPos;
+           
+            if (count > 0)
+            {
+                count = count - 1;
+                SetConutText();
+
+            }
+
+        
+        }
+
+         if (other.gameObject.CompareTag("Enemy"))
+        {
+
+            other.gameObject.transform.position = new Vector4(0, 5, 4);
+
+        }
+
+        */
 
     }
-
-
 }
