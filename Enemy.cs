e2f171ae4fe9625f4308e23c5874b24a020f7b69
Davidguar83 <davidrguardiola@hotmail.es>
2021-01-21 18:40:25 +0100

    enemigo sin colision

---
+++ Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    private NavMeshAgent pathfinger;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathfinger = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pathfinger.SetDestination(target.position);
+    }
+}
