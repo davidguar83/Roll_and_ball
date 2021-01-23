edf35ce7c7cbff1e4848345caca5a0438145e357
Davidguar83 <davidrguardiola@hotmail.es>
2021-01-23 19:29:36 +0100

    modi

--- Enemy.cs
+++ Enemy.cs
@@ -11,14 +11,14 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent pathfinger;
     private Transform target;
 
-    // Start is called before the first frame update
+   
     void Start()
     {
         pathfinger = GetComponent<NavMeshAgent>();
         target = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         pathfinger.SetDestination(target.position);
